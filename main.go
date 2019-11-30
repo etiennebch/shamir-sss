56b516d9a28a67044b75041ab56e71ae2e576555
etiennebch <et.bouche@gmail.com>
2019-12-01 00:58:36 +0100

    include sample main

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	// "fmt"
+	"encoding/hex"
+	"github.com/etiennebch/shamir-sss/shamir"
 	"log"
-
-	// "github.com/etiennebch/shamir-sss/shamir"
-	"github.com/etiennebch/shamir-sss/galois"
 )
 
 func main() {
-	field := galois.NewField256()
-	log.Print(field.Divide(249, 8))
-	// secret := []byte("secret")
-	// shares := shamir.Split(secret, 3, 2)
-	// log.Print(shares)
-	// recv := shamir.Recover(shares[0:2])
-	// log.Print(string(recv))
+	var number, threshold uint8 = 5, 3
+
+	log.Print("using Shamir to split secret value: hello world")
+	log.Printf("using number of shares: %d", number)
+	log.Printf("using threshold: %d", threshold)
+
+	shares := shamir.Split([]byte("hello world"), number, threshold)
+	for i, share := range shares {
+		log.Printf("share %d: %s", i+1, hex.EncodeToString(share))
+	}
+
+	// attempt recovery with less than threshold
+	recv := shamir.Recover(shares[:threshold-1])
+	log.Printf("attempted recovery with %d shares (threshold = %d): %s", threshold-1, threshold, string(recv))
+
+	// attempt revovery with threshold
+	recv = shamir.Recover(shares[:threshold])
+	log.Printf("attempted recovery with %d shares (threshold = %d): %s", threshold, threshold, string(recv))
 }
